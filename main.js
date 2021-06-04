86d12af02a1bff6babb337d4daa3e97b8549b310
ShivanshChokhani880 <80053639+ShivanshChokhani880@users.noreply.github.com>
2021-06-04 12:06:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+console.log("My first Console Project") ;
+
+fullname = "Shivanshchokhani"
+console.log(fullname);
+num_1 = 10 ;
+num_2 = 5 ;
+addition = num_1 + num_2 ;
+subtraction = num_1 - num_2 ;
+multiplication = num_2 * num_1 ;
+division = num_1/num_2 ; 
+console.log("number 1 = "+num_1);
+console.log("number 2 = "+num_2);
+console.log("Addition of numbers =" + addition);
+console.log("subtraction of numbers =" + subtraction);
+console.log("multiplication of numbers =" + multiplication);
+console.log("division of numbers =" + division);
+
+
